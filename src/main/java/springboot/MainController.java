a4b6f6499ab5cae14d43996727caa367c85706b3
Aman Khare <aman.khare@Infoedge.com>
2018-02-08 22:32:41 +0530

    making changes by removing unused lines....

--- src/main/java/springboot/MainController.java
+++ src/main/java/springboot/MainController.java
@@ -1,9 +1,6 @@
 package springboot;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
